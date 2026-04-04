b8f86bc1750f8555fbd371b8eeadb9c96bd1713d
Mayankmehta12 <bloggersaabji@gmail.com>
2026-04-04 10:22:17 +0530

    fix cloudinary

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,8 +2,8 @@ import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
   cloud_name: "drow8vapk",
-  api_key: "479457514713266",
-  api_secret: "L5f1-XgwrNa8jdwwnmK9HnyEesw"
+  api_key: "769715237487454",
+  api_secret: "0RhZlN8zQKcdfKKjtaX8Vk8_YF8"
 });
 
 export default cloudinary;
